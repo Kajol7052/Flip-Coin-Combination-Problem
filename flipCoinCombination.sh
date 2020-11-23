430d5ea4c0b7a3272fab527b3877c05b440ed928
“Kajol7052” <kajol7052@gmail.com>
2020-11-23 09:53:48 +0530

    Added : Randomly display Head or Tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,15 @@
 #!/bin/bash/
 echo "Welcome to Flip Coin Combination Problem"
+
+#constants
+HEAD=0
+
+#UC-1 : Flip a coin and randomly display Head or Tail
+
+flipCoin=$((RANDOM%2))
+if [ $flipCoin == $HEAD ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
